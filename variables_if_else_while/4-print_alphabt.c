729340ca7862819b8cf64b7659aaa5fb3034b808
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-20 15:05:05 -0400

    Fixed file 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,14 +11,13 @@ int main(void)
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
+	{
 		if (a == 'e' || a == 'q')
 		{
 			a++;
 		}
-		else
-		{
-			putchar (a);
-		}
+		putchar (a);
+	}
 	putchar('\n');
 	return (0);
 }
