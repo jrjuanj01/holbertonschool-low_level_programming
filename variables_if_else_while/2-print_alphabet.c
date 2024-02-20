a532ca0146be9398a72bfa21b989636925490662
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-20 14:12:19 -0400

    Fixed file 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
- * main - Entry Point
- * Main: This function prints the lowercase alphabet
+
+/**
+ * main- comment
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char a;
