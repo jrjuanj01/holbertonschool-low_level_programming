fc4323760bf337a1fe5f558407dc8161d8c6a7c7
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-22 14:50:46 -0400

    Added file 6 and updated main.h

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Returns the absolute value of int
+ * n: integer being verified
+ * Return: n or -n (Success)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
