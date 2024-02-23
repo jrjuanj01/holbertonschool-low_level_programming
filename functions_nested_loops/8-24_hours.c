5af90adb5efe8d78db92b5ed790d97c427f7ba96
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-23 12:11:56 -0400

    Added file 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints from 0 to 23:59
+ *
+ * Return: Alawys 0 (Success)
+ */
+void jack_bauer(void)
+{
+	char h1 = 0;
+	char h2 = 0;
+	char m1 = 0;
+	char m2 = 0;
+
+	for (m2 = 0; m2 <= '9'; m2++)
+	{
+		if (h1 == '2' && h2 == '4')
+		{
+			break;
+		}
+		_putchar(h1);
+		_putchar(h2);
+		_putchar(':');
+		_putchar(m1);
+		_putchar(m2);
+		_putchar('\n');
+		if (m2 == '9')
+		{
+			m2 = '/';
+			m1++;
+		}
+		if (m1 == '6')
+		{
+			m1 = '/';
+			h2++;
+		}
+		if (h2 == '9')
+		{
+			h2 = '0';
+			h1++;
+		}
+	}
+}
