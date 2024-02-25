5475e3562703d9210dd4cef9a359ec57a8284277
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-25 00:20:28 -0400

    Added file 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - displays the multiplications table
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+	int mult;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			mult = (a * b);
+			if (b == 0)
+			{
+				_putchar(' ');
+				_putchar((mult%10) +'0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (b > 0 && b < 9)
+			{
+				_putchar((mult/10) +'0');
+				_putchar((mult%10) +'0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (b == 9)
+			{
+				_putchar((mult/10) +'0');
+				_putchar((mult%10) +'0');
+			}
+		}
+		_putchar('\n');
+	}
+}
