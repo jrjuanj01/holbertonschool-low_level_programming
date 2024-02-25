e836cc9df5f9ae21cfa31037665fa7c6411308b0
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-25 04:36:41 -0400

    Added file 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * print_to_98 - Prints every number from n to 98
+ * @n: Starting point of the function
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n > 98 && n < 100)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			n--;
+		}
+		if (n >= 100)
+		{
+			_putchar((n / 100) + '0');
+			_putchar(((n / 10) % 10) + '0');
+			_putchar((n % 10) + '0');
+			n--;
+		}
+		if (n >=  0 && n < 98)
+		{
+			if (n >= 10)
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			n++;
+		}
+		if (n < 0)
+		{
+			_putchar('-');
+			if (n <= -10)
+				_putchar(((n * -1) / 10) + '0');
+			_putchar(((n * -1) % 10) + '0');
+			n++;
+		}
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar((n / 10) + '0');
+	_putchar((n % 10) + '0');
+	_putchar('\n');
+}
