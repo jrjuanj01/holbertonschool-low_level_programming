583e4a2600e9d655f83529fda83e04c121326aa7
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-25 00:50:11 -0400

    Fixed file 9 again

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,7 +15,12 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			mult = (a * b);
-
+			if ((((mult / 10) + '0') == '0') && (b == 0))
+			{
+				_putchar((mult % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
 			if ((((mult / 10) + '0') == '0') && (b < 9))
 			{
 				_putchar(' ');
