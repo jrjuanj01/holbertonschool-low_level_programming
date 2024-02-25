f62c03d6f3df944be7f79ca0ae29cead9965f1e8
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-25 02:21:47 -0400

    Fix 4 file 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,40 +15,21 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			mult = (a * b);
-			if ((((mult / 10) + '0') == '0') && (b == 0))
+			if (b > 0)
 			{
-				_putchar((mult % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			if ((((mult / 10) + '0') == '0') && (b > 0) && (b < 9))
-			{
-				_putchar(' ');
-				_putchar((mult % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			if ((((mult / 10) + '0') > '0') && (b < 9))
-			{
-				_putchar((mult / 10) + '0');
-				_putchar((mult % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (b == 9)
-			{
-				if (((mult / 10) + '0') > '0')
-				{
-					_putchar((mult / 10) + '0');
-					_putchar((mult % 10) + '0');
-				}
 				if (((mult / 10) + '0') == '0')
-				{
 					_putchar(' ');
-					_putchar((mult % 10) + '0');
-				}
+				else
+					_putchar((mult / 10) + '0');
 			}
+			_putchar((mult % 10) + '0');
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
