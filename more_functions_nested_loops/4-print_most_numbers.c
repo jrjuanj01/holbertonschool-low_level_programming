dcdb425d42f0856d3b26ecfc4e77b8cfd5363636
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-26 11:07:04 -0400

    Added files 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 - 9 except 2 & 4
+ *
+ * Return; Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int d;
+
+	for (d = '0', d <= '9', d++)
+		if (d != '2' && d != '4')
+			_putchar(d);
+	_putchar('\n');
+}
