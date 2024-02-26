b4fb49fa07546e8faa66838e99b3c8c9e56e21c3
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-26 17:57:10 -0400

    Added file 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * Fizz_buzz - prints 1-100 replacing three and five
+ *
+ * Return: Always 0 (Success)
+ */
+void Fizz_buzz(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0)
+			printf("%s", "Fizz");
+		if (num % 5 == 0)
+			printf("%s", "Buzz");
+		if (num % 3 != 0 && num % 5 != 0)
+			printf("%d", num);
+		_putchar(' ');
+	}
+}
