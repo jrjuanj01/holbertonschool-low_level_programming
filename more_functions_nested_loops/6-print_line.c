3db90ae3d9d3c21743d3dbe6e03f468d2bdc2eab
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-26 11:58:16 -0400

    Added file 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - Prints a line the length of n
+ * @n: length of line
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	for (;n > 0; n--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
