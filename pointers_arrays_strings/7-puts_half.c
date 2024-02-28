d2236530cc135a467d5dc28f909f1efa5f436a0d
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-28 11:27:02 -0400

    Fix 3 for file 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,14 +13,13 @@ void puts_half(char *str)
 	{}
 	if (ttl % 2 == 1)
 	{
-		for (mid = ((ttl /2 ) - 1); mid < ttl ; mid++)
+		for (mid = ((ttl / 2) - 1); mid <= ttl ; mid++)
 			_putchar(str[mid]);
 	}
 	if (ttl % 2 == 0)
 	{
-		for (mid = (ttl / 2); mid <= ttl ; mid++)
+		for (mid = (ttl / 2); mid < ttl ; mid++)
 			_putchar(str[mid]);
 	}
 	_putchar('\n');
 }
-
