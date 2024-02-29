645e697352692c5c7a4826c6f6ab20cf42d56070
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-29 11:16:41 -0400

    Added file 0-strcat and updated main.h

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcat - Appends the the new string into another
+ * @src: the source string
+ * @dest: the destination string
+ * Return: char pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	int ttl;
+	int idx;
+
+	for (ttl = 0; dest[ttl] != '\0'; ttl++)
+	{}
+	dest[ttl] = ' ';
+
+	for (idx = 0; src[idx] != '\0'; idx++, ttl++)
+	{
+		dest[ttl] = src[idx];
+	}
+	return (dest);
+}
