4e3879de6034ed468b1c5e919c6088843f6e3c34
Juan-Mendez <jrjuanj01@gmail.com>
2024-02-29 11:46:08 -0400

    Fixed file 1-strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -3,7 +3,7 @@
  * _strncat - Appends the the new string into another until n is reached
  * @src: the source string
  * @dest: the destination string
- * @n - maximum amount of bytes to be reached
+ * @n:  maximum amount of bytes to be reached
  * Return: char pointer
  */
 char *_strncat(char *dest, char *src, int n)
@@ -14,7 +14,6 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (ttl = 0; dest[ttl] != '\0'; ttl++)
 	{}
-
 	for (apr = 0, idx = 0; (src[idx] != '\0') && (apr < n) ; idx++, ttl++, apr++)
 	{
 		dest[ttl] = src[idx];
