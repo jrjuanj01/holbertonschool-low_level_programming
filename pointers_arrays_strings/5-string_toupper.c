1de69df37182ddb6634a6f09c2b40a63ce340ad8
Juan-Mendez <jrjuanj01@gmail.com>
2024-03-01 12:38:19 -0400

    Added file 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * sring_toupper - Converts to uppercase all lowercase charcters
+ * @str : string being verified
+ * Return: string
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	for ( ;*ptr != '\0'; ptr++)
+		if (*ptr >= 'a' && *ptr <= 'z')
+			*ptr -= 32;
+	return (ptr);
+}
