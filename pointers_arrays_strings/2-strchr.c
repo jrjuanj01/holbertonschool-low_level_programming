cf4556670e47b218c2bdc49e191447a01959c46a
Juan-Mendez <jrjuanj01@gmail.com>
2024-03-04 11:06:06 -0400

    Added file 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strchr - Finds a given charactrer
+ * @s: string being evaluated
+ * @c: Character being searched for
+ * Return: *s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c )
+			return (s);
+	}
+	return ('\0');
+}
