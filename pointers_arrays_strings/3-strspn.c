b9fd60d3407302991d99ffe8905180d8e8d62d6a
Juan-Mendez <jrjuanj01@gmail.com>
2024-03-05 02:18:55 -0400

    added file 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strspn - calculates the length of s where there are no accept characters
+ * @s: string being evaluated
+ * @accept: characters being searched for
+ * Return: int # of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int byte;
+	int idx;
+
+	for (byte = 1; *s != '\0'; s++, byte++)
+	{
+		for (idx = 0; accept[idx] != '\0'; idx++)
+		{
+			if (*s != accept[idx])
+				break;
+			return (byte);
+		}
+	}
+	return (byte);
+}
