72a53cee907e3b6488d1c86f84b2085fdadf2563
Juan-Mendez <jrjuanj01@gmail.com>
2024-03-07 11:28:35 -0400

    Added file 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints th ename of the file
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
