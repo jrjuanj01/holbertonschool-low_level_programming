a4a63b410cc9be75a311c0c2c9c5b7b0d18c7477
Juan-Mendez <jrjuanj01@gmail.com>
2024-03-10 16:24:39 -0400

    Fix 6 file 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * _calloc - allocates memory for an array of nmemb elements of size bytes each
  * @nmemb: elements
@@ -9,6 +10,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+
 	unsigned int ttl = nmemb * size;
 
 	if (nmemb == 0 || size == 0)
@@ -16,5 +18,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(ttl);
 	if (ptr == NULL)
 		return (NULL);
+	memset(ptr, 0, ttl);
 	return (ptr);
 }
