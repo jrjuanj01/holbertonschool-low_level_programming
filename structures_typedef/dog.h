3f5898cc0aeec69fb1174bb7c79979596e49366d
Juan-Mendez <jrjuanj01@gmail.com>
2024-03-11 11:40:02 -0400

    Added file dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure with dog info
+ * @name: dog name
+ * @age: dog age in years
+ * @owner: the person who owns the dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+
+#endif /* DOG_H */
