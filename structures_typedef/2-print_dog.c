b55e678a0edd209a619d91abbfbf3cd785ef849e
Juan-Mendez <jrjuanj01@gmail.com>
2024-03-11 12:50:18 -0400

    Fixed file 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ void print_dog(struct dog *d)
 	if ((*d).age == '\0')
 		printf("Age: (nil)\n");
 	else
-		printf("Age: %.1f\n", (*d).age);
+		printf("Age: %.6f\n", (*d).age);
 
 	if ((*d).owner == NULL)
 		printf("Owner: (nil)\n");
