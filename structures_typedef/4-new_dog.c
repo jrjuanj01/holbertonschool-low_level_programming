55d0623e3c3af86f5b562e8c2f1e3bafef7dadd4
Juan-Mendez <jrjuanj01@gmail.com>
2024-03-12 14:47:32 -0400

    Added file 4 and updated dog.h

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -45,6 +45,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if ((*new_dog).owner == NULL || (*new_dog).name == NULL)
 	{
+		free(name);
+		free(owner);
 		free(new_dog);
 		return (NULL);
 	}
