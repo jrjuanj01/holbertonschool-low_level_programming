95d8295ac5efd65d175e0d83be4406d0a7a9e5eb
Juan-Mendez <jrjuanj01@gmail.com>
2024-03-12 23:43:49 -0400

    Added file 5 and updated dog.h

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog -  a function that frees dogs
+ * @d: pointer to dog info to be freed
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
