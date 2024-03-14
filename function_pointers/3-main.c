c1919b3964e86cc2086970350e6d04a33d7f7e72
Juan-Mendez <jrjuanj01@gmail.com>
2024-03-14 13:22:10 -0400

    Added file 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,25 +11,26 @@ int main(int argc, char *argv[])
 {
 	int ttl;
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	ttl = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	
 	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
+
+	ttl = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 
 	printf("%d\n", ttl);
 	return (0);
