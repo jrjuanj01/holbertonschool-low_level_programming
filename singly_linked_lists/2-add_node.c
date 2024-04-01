35654de6c2a1fd5f1f1faa8db38afa07d93ed77a
Juan-Mendez <jrjuanj01@gmail.com>
2024-04-01 12:59:05 -0400

    Added a string length helper function to file 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,19 @@
 #include "lists.h"
-#include <string.h>
+
+/**
+ * _strlen - Calculates the length of the string
+ * @s: The stirng being measured
+ * Return: int (Success)
+ */
+int _strlen(const char *s)
+{
+	int l;
+
+	for (l = 0; s[l] != '\0'; l++)
+	{}
+	return (l);
+}
+
 /**
  * add_node - a function that adds a new node at the beginning of a list
  * @head: pointer of the new node
@@ -15,7 +29,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 	(*new).str = strdup(str);
-	(*new).len = strlen(str);
+	(*new).len = _strlen(str);
 	(*new).next = *head;
 	*head = new;
 	return (*head);
