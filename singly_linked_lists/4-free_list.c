cba810cdc4e9dfb60078d6e2d2872debcf6b283a
Juan-Mendez <jrjuanj01@gmail.com>
2024-04-01 19:10:54 -0400

    Added file 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: the pointer to the beginning of the list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = (*head).next;
+		free(ptr);
+	}
+}
