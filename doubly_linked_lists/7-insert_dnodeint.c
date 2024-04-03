87f6ce1b598d759600a43b193949729468fff6aa
Juan-Mendez <jrjuanj01@gmail.com>
2024-04-03 16:44:33 -0400

    Fixed and debugged file 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -8,40 +8,28 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int apr, nodes;
 	dlistint_t *new, *temp = *h;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--, temp = (*temp).next)
+	{
+		if (temp == NULL)
+			return (NULL);
+	}
+
+	if ((*temp).next == NULL)
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
-	if (*h == NULL)
-	{
-		(*new).next = NULL;
-		(*new).prev = NULL;
-		(*new).n = n;
-		*h = new;
-		return (*h);
-	}
-
-	for (nodes = 0; (*temp).next != NULL; nodes++, temp = (*temp).next);
-	if (idx > nodes)
-		return (NULL);
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	if (idx == nodes)
-		return (add_dnodeint_end(h, n));
-
-	for (apr = 0; apr <= idx; apr++)
-		temp = (*temp).next;
-
-	if (temp != NULL)
-	{
-		(*new).n = n;
-		(*new).prev = temp;
-		(*new).next = (*temp).next;
-		(*temp).next = new;
-		(*(*new).next).prev = new;
-	}
+	(*new).n = n;
+	(*new).prev = temp;
+	(*new).next = (*temp).next;
+	(*temp).next = new;
+	(*(*new).next).prev = new;
 	return (new);
 }
