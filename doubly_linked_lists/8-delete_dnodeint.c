3a3dce550363840684af980eee7c6474b2d26ee7
Juan-Mendez <jrjuanj01@gmail.com>
2024-04-04 13:59:55 -0400

    Test 2 for edge case #7 of file 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -37,8 +37,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			*head = (**head).next;
 			(**head).prev = NULL;
 			free(temp);
+			return (1);
 		}
-		return (1);
+		return (-1);
 	}
 	else
 	{
@@ -50,6 +51,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		(*(*temp).next).prev = (*temp).prev;
 		(*(*temp).prev).next = (*temp).next;
 		free(temp);
+		return (1);
 	}
 	return (1);
 }
