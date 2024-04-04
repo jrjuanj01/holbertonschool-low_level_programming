3122630b514cd740870023bbe1f1147796086bb6
Juan-Mendez <jrjuanj01@gmail.com>
2024-04-04 15:46:38 -0400

    Test 5 for edge case #7 of file 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,20 +1,5 @@
 #include "lists.h"
 /**
- * dlistint_len - a function that counts all the elements of a list
- * @h: pointer to the head of the list to be counted
- * Return: size_t number of nodes
- */
-
-size_t dlistint_len(const dlistint_t *h)
-{
-	size_t nodes;
-
-	for (nodes = 0; h != NULL; nodes++, h = (*h).next)
-	{}
-	return (nodes);
-}
-
-/**
  * delete_dnodeint_at_index - deletes the node at index
  * @head: pointer to the beginning of the list
  * @index: location of the node
@@ -23,13 +8,8 @@ size_t dlistint_len(const dlistint_t *h)
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp;
-	size_t len;
 
 	if (head == NULL)
-		return (-1);
-
-	len = dlistint_len(*head);
-	if (index > len)
 		return (-1);
 
 	if (index == 0)
@@ -48,7 +28,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		for (temp = *head; index != 0; index--, temp = (*temp).next);
+		for (temp = *head; index != 0; index--, temp = (*temp).next)
+		{
+			if (temp == NULL)
+				return (-1);
+		}
 
 		(*(*temp).next).prev = (*temp).prev;
 		(*(*temp).prev).next = (*temp).next;
