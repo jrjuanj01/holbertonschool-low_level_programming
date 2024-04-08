f7c0e3f69d0706c50aa3a348fa967cf965fe9165
Juan-Mendez <jrjuanj01@gmail.com>
2024-04-08 01:31:26 -0400

    Test 4 for file 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,28 +15,28 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return (97);
 	}
-
 	og_file = open(argv[1], O_RDONLY);
 	new_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-
-	for (;(lib = read(og_file, buffer, sizeof(buffer))) > 0;
-			ltw = write(new_file, buffer, lib))
+	if (og_file == -1)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
+	}
+	while ((lib = read(og_file, buffer, sizeof(buffer))) > 0)
+	{
+		ltw = write(new_file, buffer, lib);
 		if (new_file == -1 || lib != ltw)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			return (99);
 		}
 	}
-
-	if (lib == -1 || og_file == -1)
+	if (lib == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		return (98);
 	}
-
-
 	if (close(og_file) == -1 || close(new_file) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd \n");
