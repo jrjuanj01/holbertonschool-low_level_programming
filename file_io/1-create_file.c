01a23f6729316099acec2679c23f13263e82b73b
Juan-Mendez <jrjuanj01@gmail.com>
2024-04-08 00:05:26 -0400

    Fixed permision for file 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,10 +1,14 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 /**
  * create_file - a function that creates a file
  * @filename: name of the file to create
  * @text_content: string to write to the file
- * Returns: 1 on success, -1 on failure
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
@@ -13,20 +17,21 @@ int create_file(const char *filename, char *text_content)
 	char *reader;
 
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if(file == -1)
+	if (file == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (lib = 0, reader = text_content; ; reader++, lib++);
+		for (lib = 0, reader = text_content; *reader; reader++, lib++)
+			;
 		ltw = write(file, text_content, lib);
 	}
 
 	if (close(file) == -1 || ltw != lib)
 		return (-1);
-	
+
 	return (1);
 }
